52b6497a49afe8969515a70bbfcf32359b285ebd
alwaystiys <365825701@qq.com>
2024-08-04 03:32:26 +0800

    add create project ui and function

---
+++ PrimalEditor/Utilities/Serializer.cs
@@ -0,0 +1,49 @@
+﻿using PrimalEditor.GameProject;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.DataContracts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimalEditor.Utilities
+{
+    public static class Serializer
+    {
+        public static void ToFile<T>(T instance, string path)
+        {
+            try
+            {
+                using var fs = new FileStream(path, FileMode.Create);
+                var serializer = new DataContractSerializer(typeof(T));
+                serializer.WriteObject(fs, instance);
+
+            }
+            catch (Exception ex) 
+            {
+                Debug.WriteLine(ex.Message);
+                // TODO: Log error
+            }
+        }
+
+        public static T fromFile<T>(string path)
+        {
+            try
+            {
+                using var fs = new FileStream(path, FileMode.Open);
+                var serializer = new DataContractSerializer(typeof(T));
+                T instance = (T)serializer.ReadObject(fs);
+                return instance;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                // TODO: Log error
+            }
+            return default(T);
+        }
+    }
+}
